4dddc946b7b8d2f9787ffd8b2b62deb8064388ba
dismas <afubwa@gmail.com>
2023-01-15 12:19:40 +0400

    Javascript looping Code

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function writeCards(arrayOfNames, event = "birthday") {
+    let arrayOfGreeting = [];
+  
+    for (let name of arrayOfNames) {
+      arrayOfGreeting.push(
+        `Thank you, ${name}, for the wonderful ${event} gift!`
+      );
+    }
+  
+    return arrayOfGreeting;
+  }
+  
+  let givenNames = ["Charlie", "Samip", "Ali"];
+  writeCards(givenNames, "birthday");
+  
+  
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
